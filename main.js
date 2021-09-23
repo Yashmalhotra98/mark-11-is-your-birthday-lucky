afc51f9530019ab725169ba828c7bda1fef406b7
Yashmalhotra98 <Blackhacker1998@Gmail.com>
2021-09-23 08:55:45 +0530

    refractor: removed comments from main.js file

--- main.js
+++ main.js
@@ -2,16 +2,13 @@ const birthDate = document.querySelector('#birth-date');
 const luckyNumber = document.querySelector('#lucky-number');
 const btnCheck = document.querySelector('#check-btn');
 const outputBox = document.querySelector('#output-box');
-// btnCheck.addEventListener('click', () => console.log(birthDate.value, luckyNumber.value));
 
 btnCheck.addEventListener('click', function dateSum() {
     var dateOfBirthNew = birthDate.value.replaceAll('-', "");
     dateOfBirthNew = dateOfBirthNew.split("").map(Number);
     const sum = dateOfBirthNew.reduce((partial_sum, a) => partial_sum + a, 0)
     const luckyNum = luckyNumber.value;
-    // console.log(dateOfBirthNew);
     checkBirthDate(sum, luckyNum);
-
 });
 
 function checkBirthDate(sumOfDate, luckyNumbr) {
@@ -26,18 +23,5 @@ function checkBirthDate(sumOfDate, luckyNumbr) {
 // NOTES:-
 
 // 1. Always try to modularize your code by using a& creating as many functions as possible;
-// 2. Always check the wiring of your elements & functions with the UI or Display 
-// i.e. what you're calling is actually beiing called on the screen
-
-// function replace(str, arg1,arg2){
-//     for(var i=0; i< str.length; i++)
-//     {
-//     if(arg1 == str[i]){
-//     str[i] = arg1;
-//     }}
-//     console.log(str);
-// return str;
-// }
-
-
-// abara a /
+// 2. Always check the wiring of your elements & functions with the UI or Display i.e.
+//  what you're calling is actually beiing called on the screen
